709d2ec7e6a1aafefa49cca6da31e1fdd9553019
varshum12 <varshum12@gmail.com>
2025-05-16 11:57:00 +0000

    123

--- app.py
+++ app.py
@@ -25,3 +25,40 @@ st.subheader("By Varsha Mhetre")
 # Add box for company
 company = st.text_input("Company  Name")
 
+
+
+
+# make connection between class and app
+
+@st.cache_resource(ttl= 3600)
+def Fetch_Data():
+    return StockApi(api_key  = st.secrets["API_KEY"])
+
+
+
+stock_api = Fetch_Data()
+
+
+
+
+# search symbol
+
+@st.cache_data(ttl = 3600)
+def get_symbol(compnay):
+    symbol = stock_api.search_symbol(company)
+    return symbol
+
+
+
+@st.cache_data(ttl =  3600)
+def plot_chart(symbol):
+    df = stock_api.time_series_daily_data(symbol)
+    fig  = stock_api.plot_graph(df)
+    return fig
+
+
+if compnay:
+
+    company_data  = get_symbol(company)
+
+    symbols  =  st.selectbox(company_data)
